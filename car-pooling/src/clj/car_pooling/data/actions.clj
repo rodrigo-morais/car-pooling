80d19d95b163f7fc93f20800a8a7a717dbda8a5e
Rodrigo Moreira de Morais <morais.rm@gmail.com>
2020-03-18 15:58:47 -0300

    Preparing data to have cars availability and connect in a journey the group of people and a car.

--- car-pooling/src/clj/car_pooling/data/actions.clj
+++ car-pooling/src/clj/car_pooling/data/actions.clj
@@ -2,7 +2,7 @@
   (:require [car-pooling.data.core :as db]))
 
   (defn load-cars [cars]
-    (swap! db/*data* assoc :cars cars))
+    (swap! db/*data* assoc :cars (map (fn [car] (assoc car :available true)) cars)))
 
   (defn- get-journeys-ids []
     (vec (map (fn [journey] (get journey :id)) (:journeys @db/*data*))))
@@ -11,7 +11,7 @@
     (boolean (some #(= id %) (get-journeys-ids))))
 
   (defn add-journey [journey]
-    (swap! db/*data* assoc :journeys (conj (:journeys @db/*data*) journey)))
+    (swap! db/*data* assoc :journeys (conj (:journeys @db/*data*) (assoc journey :car nil))))
 
   (defn drop-off-journey [id]
     (swap! db/*data* assoc :journeys (remove #(= (:id %) id) (:journeys @db/*data*))))
