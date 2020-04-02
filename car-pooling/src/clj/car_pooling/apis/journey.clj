1cbc478f759c5d1ee14b6abc25bd0da27a5794f9
Rodrigo Moreira de Morais <morais.rm@gmail.com>
2020-04-02 12:50:09 -0300

    Investigating error in the acceptance test to POST/ locate.

--- car-pooling/src/clj/car_pooling/apis/journey.clj
+++ car-pooling/src/clj/car_pooling/apis/journey.clj
@@ -33,11 +33,11 @@
             (let [car (ac/get-journey-car _id)
                   is-waiting? (nil? car)]
               (if is-waiting?
-                {:status 204}
+                {:status 204 :body {:id 0}}
                 (do
                   {:status 200 :body {:id car}})))
-            {:status 404})
-          {:status 400 :body {}}))
+            {:status 404 :body {:id 0}})
+          {:status 400 :body {:id 0}}))
       (catch Exception ex
-        {:status 400 :body {}})))
+        {:status 400 :body {:id 0}})))
 
